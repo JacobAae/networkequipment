26f310cfe2c2dda90e2ae98e58b814d2f2bdec52
Jacob Mikkelsen <jacob@gennemtaenkt.dk>
2015-07-25 16:26:54 +0200

    Creates profiles and logical interfaces at atsrtup

--- grails-app/init/BootStrap.groovy
+++ grails-app/init/BootStrap.groovy
@@ -1,7 +1,36 @@
+import dk.rfit.LogicalInterface
+import dk.rfit.Profile
+
 class BootStrap {
 
-    def init = { servletContext ->
-    }
-    def destroy = {
-    }
+	def init = { servletContext ->
+
+		if( !Profile.count() ) {
+			[
+					"Backbone link",
+					"Core link",
+					"Core link mikrotik",
+					"Servernet",
+					"Bruger"
+			].each {
+				Profile profile = new Profile(profileName: it, active: true)
+				profile.save(failOnError: true)
+			}
+		}
+		if( !LogicalInterface.count() ) {
+			[
+					"access",
+					"handel",
+					"video",
+					"h11access",
+					"mgmt"
+			].each {
+				LogicalInterface logicalInterface = new LogicalInterface(name: it, active: true)
+				logicalInterface.save(failOnError: true)
+			}
+		}
+
+	}
+	def destroy = {
+	}
 }
